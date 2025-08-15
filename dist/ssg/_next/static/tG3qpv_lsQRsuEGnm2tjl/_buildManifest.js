cd20f2495453834efe1fadf94c75f3ca53eee0fb
Oglama <181064963+oglama@users.noreply.github.com>
2025-08-15 18:25:05 +0300

    [release] Version 0.0.17
    # August 15, 2025
    [code] Improved $.doQueryParent, $.doQuerySiblings, $.doQueryAt
    [code] Improved IO docs
    [code] Human readable run time
    [code] Added run datetime to <FileList/>
    [code] Added run datetime to tables
    [code] Added run time to <TableField/>
    [code] Added custom drawer
    
    # August 14, 2025
    [code] Updated settings and results filters

---
+++ dist/ssg/_next/static/tG3qpv_lsQRsuEGnm2tjl/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama (https://oglama.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,c,e,t,a){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,c,e,t,"static/chunks/570-ac43b52eb56b2de9.js",a,"static/chunks/pages/index-be4ebbe570eec5bd.js"],"/404":["static/chunks/pages/404-6555439f0ebbaca5.js"],"/500":["static/chunks/pages/500-1ab75baae40b66fb.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/home":["static/chunks/pages/home-c9f60736a572ad71.js"],"/home/test":[s,c,t,"static/chunks/945-51bbd94132286e16.js","static/chunks/pages/home/test-b5cdd561b9ca719d.js"],"/login":[s,"static/chunks/pages/login-c9df2972a99e2760.js"],"/source-code":[s,c,e,"static/chunks/353-b0e7963281562583.js",a,"static/chunks/pages/source-code-c2b15c64021f7c3a.js"],sortedPages:["/","/404","/500","/_app","/_error","/_offline","/home","/home/test","/login","/source-code"]}}("static/chunks/901-0c929d65b96cc575.js","static/chunks/884-214346365836ebca.js","static/chunks/48-4d11640bda7b8e52.js","static/chunks/991-eb6144fc2108f938.js","static/chunks/955-cb887f3bf1bac88f.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
