43aec8586bd08722006210cfe3f1c82c0f75d87a
Oglama <181064963+oglama@users.noreply.github.com>
2025-09-04 17:35:40 +0300

    [release] Version 0.0.30
    # September 4, 2025
    [code] Improved debouncer efficiency
    [code] Debounce trigger checks
    [code] Improved TabTriggers UX; fixed sessionRunCount issue
    [code] Fixed runId bug in Triggers
    
    # September 3, 2025
    [code] Triggers - ready flags
    [code] Added <CronField/>
    [code] Isolated session details modifiers
    [code] Tasks to Triggers
    
    # September 2, 2025
    [code] Editable task name
    [code] Added <SessionsList/>
    [code] Added Oglama_Ipc_Session
    
    # August 31, 2025
    [code] Prepared session CRUD
    [code] Improved <TabResultsItemBoolean/>
    
    # August 30, 2025
    [code] Improved results UI
    [code] Updated SubscriptionFragment
    
    # August 29, 2025
    [code] Improved diskStorage DevEx
    [code] Added support for custom sessions
    [code] Slower, more realistic typing
    [code] Prepared list for triggers
    
    # August 28, 2025
    [code] Triggers baby steps
    [code] Dynamic purging of modules

---
+++ dist/ssg/_next/static/xHG6e4AqiXoUp-YUvFBtK/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama (https://oglama.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,c,e,t,a){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,c,e,t,"static/chunks/857-79527af557212f2c.js",a,"static/chunks/pages/index-c081296c44f443c6.js"],"/404":["static/chunks/pages/404-6555439f0ebbaca5.js"],"/500":["static/chunks/pages/500-1ab75baae40b66fb.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/home":["static/chunks/pages/home-0859eaf839376928.js"],"/home/test":[s,c,t,"static/chunks/945-51bbd94132286e16.js","static/chunks/pages/home/test-b5cdd561b9ca719d.js"],"/login":[s,"static/chunks/pages/login-c9df2972a99e2760.js"],"/source-code":[s,c,e,"static/chunks/83-d959b6b87e984c46.js",a,"static/chunks/pages/source-code-ad8215ff5ba99715.js"],sortedPages:["/","/404","/500","/_app","/_error","/_offline","/home","/home/test","/login","/source-code"]}}("static/chunks/901-0c929d65b96cc575.js","static/chunks/884-214346365836ebca.js","static/chunks/899-5532c74750a2fe07.js","static/chunks/991-eb6144fc2108f938.js","static/chunks/955-97c70e93e0102e5a.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
