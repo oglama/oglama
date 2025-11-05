bd1add47ac5ef1c8d837c04358613ffd0e57fd67
Oglama <181064963+oglama@users.noreply.github.com>
2025-11-05 16:39:36 +0200

    [release] Version 0.0.64
    # November 5, 2025
    [code] Added multiple property to input files
    [code] Improved UX in session dialog

---
+++ dist/ssg/_next/static/VO80DqQbcFeBOVgGv2a_k/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama (https://oglama.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,e,t,c,a,n,i,r){return{__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},__routerFilterStatic:{numItems:0,errorRate:1e-4,numBits:0,numHashes:null,bitArray:[]},__routerFilterDynamic:{numItems:s,errorRate:1e-4,numBits:s,numHashes:null,bitArray:[]},"/":[e,t,n,i,"static/chunks/492-729e6363c4a607a2.js",r,"static/chunks/pages/index-766b91c08dcf1904.js"],"/404":["static/chunks/pages/404-dc2de4c822b4de18.js"],"/500":["static/chunks/pages/500-00d02b53bd790431.js"],"/_error":["static/chunks/pages/_error-c99528965161d4de.js"],"/_offline":["static/chunks/pages/_offline-d0760282e5bbff8d.js"],"/home":["static/chunks/pages/home-eff82049584b576e.js"],"/home/test":[e,t,i,"static/chunks/298-1e31f22b15ec72d7.js","static/chunks/pages/home/test-11e2e9008fcc400f.js"],"/login":[e,"static/chunks/pages/login-b14db1379ff97df1.js"],"/source-code":[e,t,n,"static/chunks/864-541f3b151e0bab0e.js",r,"static/chunks/pages/source-code-5f9782ecba83094e.js"],sortedPages:["/","/404","/500","/_app","/_error","/_offline","/home","/home/test","/login","/source-code"]}}(0,"static/chunks/894-0d11472c0b0003b5.js","static/chunks/957-b71da0efcdc9f3d9.js",1e-4,null,"static/chunks/689-d377f36f81bf01dd.js","static/chunks/316-0385b075274ef933.js","static/chunks/338-adb714620d0a718a.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
